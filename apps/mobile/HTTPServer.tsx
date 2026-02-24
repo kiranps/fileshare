835a66c9117de7e422cceeaf84f0aa6125829117
Kiran PS <pskirann@gmail.com>
2026-02-24 18:25:14 +0530

    Move init of WebDavServer outside component

--- apps/mobile/HTTPServer.tsx
+++ apps/mobile/HTTPServer.tsx
@@ -1,32 +1,40 @@
 import { Text, View, Button, StyleSheet } from "react-native";
 import { useState } from "react";
 import { WebDavServer } from "react-native-webdav-server";
+const server = new WebDavServer(8080, "/storage/emulated/0");
 
 export default function HTTPServer() {
   const [message, setMessage] = useState<string>("");
-  const server = new WebDavServer(8080, "/storage/emulated/0");
+  const [isRunning, setIsRunning] = useState(false);
 
   const handleStartServer = () => {
     server.start();
+    setIsRunning(true);
+    setMessage("Server started");
   };
 
   const handleStopServer = () => {
     server.stop();
+    setIsRunning(false);
+    setMessage("Server stopped");
   };
 
   return (
     <View style={styles.container}>
       <Text>Result: {message}</Text>
-      <Button
-        title="Start Kotlin Server"
-        onPress={handleStartServer}
-        color="green"
-      />
-      <Button
-        title="Stop Kotlin Server"
-        onPress={handleStopServer}
-        color="red"
-      />
+      {!isRunning ? (
+        <Button
+          title="Start Kotlin Server"
+          onPress={handleStartServer}
+          color="green"
+        />
+      ) : (
+        <Button
+          title="Stop Kotlin Server"
+          onPress={handleStopServer}
+          color="red"
+        />
+      )}
     </View>
   );
 }
