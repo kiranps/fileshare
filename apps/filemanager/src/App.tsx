a80489b9951d33d5203f909f990d8ecc2be83e07
Kiran PS <pskirann@gmail.com>
2026-03-07 00:53:28 +0530

    Disable right click

--- apps/filemanager/src/App.tsx
+++ apps/filemanager/src/App.tsx
@@ -1,6 +1,18 @@
 import { FileManager } from "./components/FileManager";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const disableRightClick = (e: MouseEvent) => {
+      e.preventDefault();
+    };
+
+    document.addEventListener("contextmenu", disableRightClick);
+
+    return () => {
+      document.removeEventListener("contextmenu", disableRightClick);
+    };
+  }, []);
   return <FileManager />;
 }
 
